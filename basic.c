b20f165c4350ebbd4635df4b77d3ad20285e61ea
shilinlee <836160610@qq.com>
2018-12-07 22:41:47 +0800

    Variables

--- basic.c
+++ basic.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("hello world\n");
+    int num1;
+    int num2;
+    int sum;
+
+    printf("Enter Your number: ");
+    scanf("%d", &num1); 
+    printf("Enter Your number: ");
+    scanf("%d", &num2);
+    sum = num1 + num2;
+    printf("The sum is %d\n", sum);
+
 }
