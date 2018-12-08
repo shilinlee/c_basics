320a9bd5aeb03d6511c0f35872c7cc96bc2238e0
shilinlee <836160610@qq.com>
2018-12-08 21:03:50 +0800

    Intro to Arrays

---
+++ arrays.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int shilinlee[5];  // 5 integers, default: 0
+    int zr[5] = {23, 34, 234, 22, 2345};  // 赋值
+    int zhao[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};  // 赋值
+    shilinlee[0] = 1;
+    shilinlee[1] = 2;
+
+    printf("shilinlee[1]: %d\n", shilinlee[1]);
+    printf("shilinlee[2]:%d\n", shilinlee[2]);
+
+    printf("zr[2]: %d\n", zr[2]);
+
+    printf("zhao[1]: %d\n", zhao[1]);
+    
+
+}
